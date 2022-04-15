85091e8c7d2b4340839df231717c6fd1f11b6fd2
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-15 20:55:02 +0530

    create components

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { DeleteContactComponent } from './delete-contact/delete-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { ListContactComponent } from './list-contact/list-contact.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddContactComponent,
+    DeleteContactComponent,
+    EditContactComponent,
+    ListContactComponent
+  ],
   imports: [
     CommonModule
   ]
