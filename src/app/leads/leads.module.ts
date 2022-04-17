d0f71ab1bbc94c03de261a7f9e7345da247b3950
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-17 19:00:46 +0530

    component communication

--- src/app/leads/leads.module.ts
+++ src/app/leads/leads.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LeadsListingComponent } from './leads-listing/leads-listing.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LeadsListingComponent,
+
+  ],
   imports: [
     CommonModule
   ]
