1744825e284ff90f16f9d685a39ee120537c25e1
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-19 20:23:31 +0530

    ngSwitch demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,64 @@ export class AppComponent {
   title = 'simplecrm';
   success_msg = true;
 
+  superPower = 'superwomen';//string
 
+  tax = 10;
+
+
+  contacts = [{
+    'firstname': 'meni',
+    'lastname': "menaga",
+    'id': 1234
+  },
+
+  {
+    'firstname': 'saran',
+    'lastname': 'saravanan',
+    'id': 1190
+
+  },
+  {
+    'firstname': "tamil",
+    'lastname': "tamilselvan",
+    'id': 3466
+
+  },
+  {
+    'firstname': 'saran',
+    'lastname': 'saravanan',
+    'id': 1190
+
+  },
+  {
+    'firstname': "tamil",
+    'lastname': "tamilselvan",
+    'id': 3466
+
+  },
+  {
+    'firstname': 'saran',
+    'lastname': 'saravanan',
+    'id': 1190
+
+  },
+  {
+    'firstname': "tamil",
+    'lastname': "tamilselvan",
+    'id': 3466
+
+  },
+  {
+    'firstname': 'saran',
+    'lastname': 'saravanan',
+    'id': 1190
+
+  },
+  {
+    'firstname': "tamil",
+    'lastname': "tamilselvan",
+    'id': 3466
+
+  }
+  ]
 }
