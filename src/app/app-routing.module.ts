6b601922c51b2a2c978c9bd668e7f309f329d28d
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-22 15:22:27 +0530

    com router create &demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddLoansComponent } from './add-loans/add-loans.component';
+import { LoansTypesComponent } from './loans-types/loans-types.component';
+import { LoansComponent } from './loans/loans.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'loans',
+    component: LoansComponent
+  },
+  {
+    path: 'loans-types',
+    component: LoansTypesComponent
+  },
+  {
+    path: 'loans/add-loans',
+    component: AddLoansComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { MatRadioModule } from '@angular/material/radio';
 import { HighlightDirective } from './highlight.directive';
 import { UsersComponent } from './users/users.component';
 import { FormsModule } from '@angular/forms';
+import { LoansComponent } from './loans/loans.component';
+import { LoansTypesComponent } from './loans-types/loans-types.component';
+import { AddLoansComponent } from './add-loans/add-loans.component';
 
 
 @NgModule({
@@ -17,6 +20,9 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     HighlightDirective,
     UsersComponent,
+    LoansComponent,
+    LoansTypesComponent,
+    AddLoansComponent,
 
   ],
   imports: [
