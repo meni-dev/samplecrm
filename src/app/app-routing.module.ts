d4ea029715594a3c0cc9140f9b8a3fb89fd76633
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-25 19:34:27 +0530

    canActivatechild demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router'
 import { LoansComponent } from './loans/loans.component';
@@ -8,6 +8,12 @@ import { ClientsComponent } from './clients/clients.component';
 import { LeadsGridComponent } from './leads/leads-listing/leads-grid/leads-grid.component';
 import { SearchComponent } from './search/search.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SuperAdminGuard } from './super-admin.guard';
+import { AdminComponent } from './admin/admin.component';
+import { AdminAccessGuard } from './admin-access.guard';
+import { AdminEditComponent } from './admin-edit/admin-edit.component';
+import { AdminDeleteComponent } from './admin-delete/admin-delete.component';
+import { AdminManageComponent } from './admin-manage/admin-manage.component';
 
 
 
@@ -33,10 +39,27 @@ const routes: Routes = [
   { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
   { path: 'payments', loadChildren: () => import('./payments/payments.module').then(m => m.PaymentsModule) },
   {
-    path: '**', component: PageNotFoundComponent
-  }
-];
+    path: 'admin',
+    canActivate: [SuperAdminGuard],
+    children: [
+      {
+        path: '',
+        component: AdminComponent
+      },
+      {
+        path: '',
+        canActivateChild: [AdminAccessGuard],
+        children: [
+          { path: 'edit', component: AdminEditComponent },
+          { path: 'delete', component: AdminDeleteComponent },
+          { path: 'manage', component: AdminManageComponent }
 
+        ]
+
+      },
+    ]
+  }
+]
 @NgModule({
   declarations: [],
   imports: [
