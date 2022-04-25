854e11192fab048dae70cce50c1e7564c41d8fee
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-25 13:49:30 +0530

    canActivate - Guard example

--- src/app/payments/payments-routing.module.ts
+++ src/app/payments/payments-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../auth.guard';
 import { PaymentsComponent } from './payments.component';
 
 const routes: Routes = [{ path: '', component: PaymentsComponent },
-{ path: 'succSess', component: PaymentsComponent }];
+{ path: 'succSess', component: PaymentsComponent },
+{
+  path: 'payments',
+  component: PaymentsComponent,
+  canActivate: [AuthGuard]
+}
+];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
