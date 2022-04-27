03bf4bb1e8b98031c23ba31a8ff7017e38d8d129
menisaran15@gmail.com <menisaran15@gmail.com>
2022-04-27 20:04:44 +0530

    set Formvalues demo

--- src/app/loans-types/loans-types.component.ts
+++ src/app/loans-types/loans-types.component.ts
@@ -19,10 +19,26 @@ export class LoansTypesComponent implements OnInit {
     //   'LoanDescription': new FormControl()
 
     // })
+    /* this.addLoanTypesForm = this.fb.group({
+       'LoanName': new FormControl('Saran Loan'),
+       'LoanType': new FormControl('PersonalLoan'),
+       'LoanDescription': new FormControl('This is for laptop')
+     })*/
     this.addLoanTypesForm = this.fb.group({
       'LoanName': new FormControl(),
-      'LoanType': new FormControl()
+      'LoanType': new FormControl(),
+      'LoanDescription': new FormControl()
     })
+    const newLoanObj = {
+      'LoanName': 'My Loan Application',
+      'LoanType': 'Personal Loan',
+      // 'LoanDescription': 'This is Loan Application'
+
+    }
+    //another way of setting values of forms
+    this.addLoanTypesForm.setValue(newLoanObj);
+    //the only didderence is--> you donot have to pass all  "fields/keys"
+    // this.addLoanTypesForm.patchValue(newLoanObj);
   }
   addLoanType() {
     console.log(this.addLoanTypesForm.value)
